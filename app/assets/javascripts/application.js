408e657803cf7cce7825606ef89be0fcddc0a1dd
Thomas Roeder <test@testmail.com>
2012-02-10 23:15:36 +0100

    first working fancybox stuff :-)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,26 @@
 //= require fancybox
 //= require jquery_ujs
 //= require_tree .
+
+//For setup of fancybox see :
+//https://github.com/hecticjeff/fancybox-rails
+//http://stackoverflow.com/questions/8681041/rails-3-1-fancybox
+
+//Copied from http://stackoverflow.com/questions/8681041/rails-3-1-fancybox
+$(document).ready(function(){
+ $("a.single_image").fancybox({'type': 'image'});
+});
+
+//Copied from jquery.fancybox-1.3.2.setup.js
+$(document).ready(function () {
+    $("a.fb_image").fancybox({
+        'transitionIn': 'elastic',
+        'transitionOut': 'elastic',
+        'titlePosition': 'over',
+        'speedIn': 600,
+        'speedOut': 600,
+        'overlayShow': true,
+        'overlayColor': '#000',
+        'overlayOpacity': 0.9
+    });
+});
